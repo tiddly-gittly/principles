124df0135409d3056876a0843a5afc3033326ce9
linonetwo <linonetwo012@gmail.com>
2024-10-29 19:22:24 +0800

    fix: filter

--- src/principles/deck/principles-filter.ts
+++ src/principles/deck/principles-filter.ts
@@ -1,18 +1,22 @@
-import { IFilterOperator } from "tiddlywiki";
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { IFilterOperator } from 'tiddlywiki';
 
 const lingoBase = '$:/plugins/linonetwo/principles/language/';
-export function lingo(key: string) {
+function lingo(key: string) {
   const languageCode = $tw.wiki.filterTiddlers('[[$:/language]get[text]get[name]else[en-GB]]')[0];
   return $tw.wiki.getTiddlerText(`${lingoBase}${languageCode}/${key}`, key);
 }
 
-const principlesFilter: IFilterOperator = (source,operator) => {
-	let rootTiddler = $tw.wiki.getTiddlerText('$:/plugins/linonetwo/principles/configs/RootTiddler');
-	if (!rootTiddler) {
-		rootTiddler = lingo('Name');
-	}
-	// we are getting all the tiddlers that are in the outline
-	const tiddlersInOutline = $tw.wiki.filterTiddlers(`[${rootTiddler}get-stream-nodes[]]`);
-	return tiddlersInOutline;
+const principlesFilter: IFilterOperator = (source, operator) => {
+  let rootTiddler = $tw.wiki.getTiddlerText('$:/plugins/linonetwo/principles/configs/RootTiddler');
+  if (!rootTiddler) {
+    rootTiddler = lingo('Name');
+  }
+  // we are getting all the tiddlers that are in the outline
+  const tiddlersInOutline = $tw.wiki.filterTiddlers(`[[${rootTiddler}]get-stream-nodes:exclusive[]]`);
+  return tiddlersInOutline;
 };
-exports['linonetwo.principles'] = principlesFilter
+declare let exports: {
+  'linonetwo.principles': IFilterOperator;
+};
+exports['linonetwo.principles'] = principlesFilter;
